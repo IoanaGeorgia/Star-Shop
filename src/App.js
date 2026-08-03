a841df9ce98ef1dc91d651a291586dac226e82ed
io <ioana.pascu@history.bucharest.university>
2026-08-03 15:12:16 +0300

    updated basename

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Header from './pages/Header';
-import Catalogue from './pages/Catalogue';
-import BuyStar from './pages/BuyStar';
-import Contact from './pages/Contact';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./pages/Header";
+import Catalogue from "./pages/Catalogue";
+import BuyStar from "./pages/BuyStar";
+import Contact from "./pages/Contact";
 
 function App() {
-    return (
-    <BrowserRouter> 
-    <Header />     
-    <Routes basename="/Star-Shop"> 
- 
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />      
-        <Route path='/catalogue' element={<Catalogue />} />
-        <Route path="/buy-star" element={<BuyStar />} />  
-        <Route path="/contact" element={<Contact />} />      
-
-        </Routes>
-
-    </BrowserRouter>  
-        
-);
-
+  return (
+    <BrowserRouter basename="/Star-Shop">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/buy-star" element={<BuyStar />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-            
+
 export default App;
